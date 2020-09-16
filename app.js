0e60e9ccc847893a7b2340145b2f401fd6a52864
charlesaaronj <charlesaaronj@gmail.com>
2020-09-15 22:56:08 -0500

    save

--- app.js
+++ app.js
@@ -54,12 +54,45 @@ noScroll.classList.remove('is-clipped');
 console.log(content); 
 }); 
 
-const htmlOverview = document.querySelector(".html-overview")
-console.log(htmlOverview);
-htmlOverview.addEventListener('click', () => {
-console.log('you clicked me');
-const contentOverview = document.querySelector('.html-overview-content')
-contentOverview.classList.add('is-hidden');
-const contentTutorials = document.querySelector('.html-tutorials-content')
-const contentPortfolio = document.querySelector('.html-portfolio-content')
+const htmlOver = document.querySelector(".html-over")
+const htmlTuts = document.querySelector(".html-tuts")
+const htmlPort = document.querySelector(".html-port")
+const htmltutsCont = document.querySelector('.html-tuts-cont')
+const htmlportCont = document.querySelector('.html-port-cont')
+const htmloverCont = document.querySelector('.html-over-cont')
+
+htmlTuts.addEventListener('click', () => {
+    htmltutsCont.classList.remove('is-hidden');
+    htmloverCont.classList.add('is-hidden');
+    htmlportCont.classList.add('is-hidden');
+    htmlOver.classList.remove('is-active');
+    htmlPort.classList.remove('is-active');
+    event.currentTarget.classList.add('is-active');
+
 });
+
+htmlPort.addEventListener('click', () => {
+    htmlportCont.classList.remove('is-hidden');
+    htmltutsCont.classList.add('is-hidden');
+    htmloverCont.classList.add('is-hidden');
+    htmlOver.classList.remove('is-active');
+    htmlTuts.classList.remove('is-active');
+    event.currentTarget.classList.add('is-active');
+});
+
+htmlOver.addEventListener('click', () => {
+    htmloverCont.classList.remove('is-hidden');
+    htmlportCont.classList.add('is-hidden');
+    htmltutsCont.classList.add('is-hidden');
+    htmlPort.classList.remove('is-active');
+    htmlTuts.classList.remove('is-active');
+    event.currentTarget.classList.add('is-active');
+});
+
+const cssOver = document.querySelector(".css-over")
+const cssTuts = document.querySelector(".css-tuts")
+const cssPort = document.querySelector(".css-port")
+const csstutsCont = document.querySelector('.css-tuts-cont')
+const cssportCont = document.querySelector('.css-port-cont')
+const cssoverCont = document.querySelector('.css-over-cont')
+
