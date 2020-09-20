6f62beccffbee8271718cb6dc3034b66a03b99c9
charlesaaronj <charlesaaronj@gmail.com>
2020-09-20 09:19:08 -0500

    save

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    console.log(navbarBurgers)
+
+    // Check if there are any navbar burgers
+    if (navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          console.log(target)
+          const targetTwo = document.getElementById(target);
+          console.log(targetTwo)
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          targetTwo.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
 const noScroll = document.querySelector('html');
 
 const buttonOpen1 = document.getElementById('open-1');
@@ -53,46 +82,3 @@ content.classList.remove('is-active');
 noScroll.classList.remove('is-clipped');
 console.log(content); 
 }); 
-
-const htmlOver = document.querySelector(".html-over")
-const htmlTuts = document.querySelector(".html-tuts")
-const htmlPort = document.querySelector(".html-port")
-const htmltutsCont = document.querySelector('.html-tuts-cont')
-const htmlportCont = document.querySelector('.html-port-cont')
-const htmloverCont = document.querySelector('.html-over-cont')
-
-htmlTuts.addEventListener('click', () => {
-    htmltutsCont.classList.remove('is-hidden');
-    htmloverCont.classList.add('is-hidden');
-    htmlportCont.classList.add('is-hidden');
-    htmlOver.classList.remove('is-active');
-    htmlPort.classList.remove('is-active');
-    event.currentTarget.classList.add('is-active');
-
-});
-
-htmlPort.addEventListener('click', () => {
-    htmlportCont.classList.remove('is-hidden');
-    htmltutsCont.classList.add('is-hidden');
-    htmloverCont.classList.add('is-hidden');
-    htmlOver.classList.remove('is-active');
-    htmlTuts.classList.remove('is-active');
-    event.currentTarget.classList.add('is-active');
-});
-
-htmlOver.addEventListener('click', () => {
-    htmloverCont.classList.remove('is-hidden');
-    htmlportCont.classList.add('is-hidden');
-    htmltutsCont.classList.add('is-hidden');
-    htmlPort.classList.remove('is-active');
-    htmlTuts.classList.remove('is-active');
-    event.currentTarget.classList.add('is-active');
-});
-
-const cssOver = document.querySelector(".css-over")
-const cssTuts = document.querySelector(".css-tuts")
-const cssPort = document.querySelector(".css-port")
-const csstutsCont = document.querySelector('.css-tuts-cont')
-const cssportCont = document.querySelector('.css-port-cont')
-const cssoverCont = document.querySelector('.css-over-cont')
-
