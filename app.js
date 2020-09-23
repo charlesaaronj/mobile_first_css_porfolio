3756dadf53c0d403f15ef24b79c3e6b98fd6cdcf
charlesaaronj <charlesaaronj@gmail.com>
2020-09-22 23:05:00 -0500

    save

--- app.js
+++ app.js
@@ -27,25 +27,127 @@ document.addEventListener('DOMContentLoaded', () => {
   
   });
 
-const noScroll = document.querySelector('html');
+  window.addEventListener("DOMContentLoaded", function() {
 
-const buttonOpen1 = document.getElementById('open-1');
-buttonOpen1.addEventListener('click', () => {
-console.log('you clicked me');
-const content = document.getElementById('modal-1')
-content.classList.add('is-active');
+    // get the form elements defined in your form HTML above
+    
+    const form = document.getElementById("my-form");
+    const button = document.getElementById("my-form-button");
+    const status = document.getElementById("my-form-status");
+    const hideField = document.querySelectorAll(".field");
+    const reach = document.querySelector('.reach');
+    const sent = document.querySelector('.sent');
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      hideField.forEach(myFunction);
+      function myFunction(item) {
+      item.classList.add('is-hidden');
+      }
+      reach.classList.add('is-hidden');
+      sent.classList.remove('is-hidden');
+      let data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
+  //clear contact form
+
+  const clear = document.getElementById('clear')
+  clear.addEventListener('click', () => {
+  console.log('you clicked me');
+  document.getElementById("my-form").reset();
+  });
+
+const noScroll = document.querySelector('html');
+const backgroundClose = document.querySelector('.modal-background');
+
+const open1 = document.querySelector('.open1');
+const open2 = document.querySelector('.open2');
+const open3 = document.querySelector('.open3');
+const open4 = document.querySelector('.open4');
+const open5 = document.querySelector('.open5');
+const open6 = document.querySelector('.open6');
+const open7 = document.querySelector('.open7');
+const open8 = document.querySelector('.open8');
+const open9 = document.querySelector('.open9');
+const open10 = document.querySelector('.open10');
+
+const cont1 = document.getElementById('modal1')
+const cont2 = document.getElementById('modal2')
+const cont3 = document.getElementById('modal3')
+const cont4 = document.getElementById('modal4')
+const cont5 = document.getElementById('modal5')
+const cont6 = document.getElementById('modal6')
+const cont7 = document.getElementById('modal7')
+const cont8 = document.getElementById('modal8')
+const cont9 = document.getElementById('modal9')
+const cont10 = document.getElementById('modal10')
+
+open1.addEventListener('click', () => {
+cont1.classList.add('is-active');
 noScroll.classList.add('is-clipped');
 console.log(content);
 });
 
+window.onclick = function(event) {
+  let eventar = event.target;
+  var myClass = this.className;
+  console.log(eventar, myClass)
+  }
+
+const array = ['.open1','.open2','.open3'];
+
+const found = array.indexOf('.open3');
+
+console.log(found);
+
 const buttonClose1 = document.getElementById('close-1');
+
+const content = document.getElementById('modal1')
 buttonClose1.addEventListener('click', () => {
 console.log('you clicked me');
-const content = document.getElementById('modal-1')
 content.classList.remove('is-active');
 noScroll.classList.remove('is-clipped');
-console.log(content); 
 });
+
+backgroundClose.addEventListener('click', () => {
+  console.log('you clicked me');
+  content.classList.remove('is-active');
+  noScroll.classList.remove('is-clipped');
+  });
+
 
 const buttonOpen2 = document.getElementById('open-2');
 buttonOpen2.addEventListener('click', () => {
